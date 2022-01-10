587230a521517b4f17b0f42e200490e22613fb80
BLooDBRothER <computertechnology446@gmail.com>
2022-01-10 12:35:46 +0530

    update header UI

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { MdPhotoCameraBack, MdPhotoCameraFront } from 'react-icons/md';
 
 const Header = () => {
     return (
         <header className='header'>
             <h1 className='header-title'>PixDiscovery</h1>
             <nav className='header-nav none'>
-                <a href='hi' className='header-link'>Photo</a>
-                <a href='hi' className='header-link'>Video</a>
+                <a href='hi' className='header-link'><MdPhotoCameraBack className='header-link-ic'/> Photo</a>
+                <a href='hi' className='header-link'><MdPhotoCameraFront className='header-link-ic'/>Video</a>
             </nav>
         </header>
     )
