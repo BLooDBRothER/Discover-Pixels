e704a8158aaf1808b7eeaa494032f96c2ec98350
BLooDBRothER <computertechnology446@gmail.com>
2022-01-10 12:16:32 +0530

    create header UI

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <h1 className='header-title'>PixDiscovery</h1>
+            <nav className='header-nav none'>
+                <a href='hi' className='header-link'>Photo</a>
+                <a href='hi' className='header-link'>Video</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
