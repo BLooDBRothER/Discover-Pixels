10d569e0179b8be49ee0f6932c5d082fc5ac05ff
BLooDBRothER <computertechnology446@gmail.com>
2022-01-15 12:45:40 +0530

    Create custom checkbox

---
+++ src/components/search/CheckBox.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+const CheckBox = ({value, checkboxStatue, setCheckedStatus}) => {
+    const toggleCheckbox = () => {
+        setCheckedStatus(prevState => !prevState);
+    }
+
+    return (
+        <div className='checkbox' onClick={toggleCheckbox}>
+            <div className={`checkbox-selected ${checkboxStatue ? "selected" : false}`}></div>
+            <div className='checkbox-value'>{value}</div>
+        </div>
+    )
+}
+
+export default CheckBox
