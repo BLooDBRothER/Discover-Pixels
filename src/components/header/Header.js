9a2e68e285cd44fb4335a5c0e209667a4e7666f5
BLooDBRothER <computertechnology446@gmail.com>
2022-01-17 11:06:13 +0530

    Update font family and spacings

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,7 +6,7 @@ const Header = ({isNavbarVisible}) => {
     return (
         <>
         <header className='header'>
-            <h1 className='header-title'>PixDiscovery</h1>
+            <h1 className='header-title'><span className="letter-color">P</span>ix<span className='letter-color'>D</span>iscovery</h1>
             {(isNavbarVisible && <Navbar />)}
         </header>
         <Outlet />
