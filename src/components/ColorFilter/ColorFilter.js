f9d44b113a46ebf62316fe003d4fb876523bb9ae
BLooDBRothER <computertechnology446@gmail.com>
2022-01-18 20:52:29 +0530

    Update UI and add color to the list

--- src/components/ColorFilter/ColorFilter.js
+++ src/components/ColorFilter/ColorFilter.js
@@ -1,64 +1,85 @@
-import React, {useState, useEffect, useRef} from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useState, useEffect, useContext, useRef} from 'react';
 import { FaCaretDown, FaCaretUp } from 'react-icons/fa';
 import CheckBox from '../checkbox/CheckBox';
 import ColorItem from './ColorItem';
+import { colorArrayContext } from '../search_photo/SearchPage';
+
+export const tempColorContext = React.createContext(null);
 
 const ColorFilter = () => {
-    const [selected, setSelected] = useState([]);
     const [isOpen, setIsOpen] = useState(false);
     const dropdownListRef = useRef(null);
+    const setColorArray = useContext(colorArrayContext);
 
+    const [tempColorList, setTempColorList] = useState([]);
     const [isGrayscale, setIsGrayscale] = useState(false);
     const [isTransparent, setIsTransparent] = useState(false);
-    
-    // handle outside click
-    // const handleOutsideClick = (e) => {
-    //     console.log(e);
-    //     setIsOpen(false); 
-    // }
-    // useEffect(() => {
-    //     if(!dropdownListRef.current) return;
-    //     document.addEventListener("click", handleOutsideClick);
-    //     return () => {
-    //         console.log("he")
-    //         document.removeEventListener("click", handleOutsideClick);
-    //     }
-    // });
+
+    useEffect(() => {
+        if(isGrayscale){
+            setTempColorList(prev => [...prev, "grayscale"]);
+        }
+        else{
+            const removedColorArray = tempColorList.filter(color => color !== "grayscale");
+            setTempColorList(removedColorArray);
+        }
+    }, [isGrayscale]);
+
+    useEffect(() => {
+        if(isTransparent){
+            setTempColorList(prev => [...prev, "transparent"]);
+        }
+        else{
+            const removedColorArray = tempColorList.filter(color => color !== "transparent");
+            setTempColorList(removedColorArray);
+        }
+    }, [isTransparent]);
+
+
+
+    useEffect(() => {
+        console.log(tempColorList);
+    }, [tempColorList])
 
     const toggleDropdown = () => {
-        console.log("he")
         setIsOpen(prevValue => !prevValue);
     }
-    useEffect(() => {
-        console.log(isOpen);
 
-    }, [isOpen])
+    const updateColorList = () => {
+        setColorArray(tempColorList);
+        setIsOpen(false);
+    }
+
     return (
-        <div className={`color-filter ${isOpen ? "active" : ""}`}>
-            <div className={"color-filter-title"} onClick={toggleDropdown}>
-                <h3>Colors</h3>
-                {isOpen ? <FaCaretUp className='color-filter-ic' /> : <FaCaretDown className='color-filter-ic' />}
-            </div>
-            {isOpen && 
-            (<div className='color-filter-value' ref={dropdownListRef}>
-                <CheckBox value="Grayscale" checkboxStatue={isGrayscale} setCheckedStatus={setIsGrayscale}></CheckBox>
-                <CheckBox value="Transparent" checkboxStatue={isTransparent} setCheckedStatus={setIsTransparent}></CheckBox>
-                <div className='color-filter-palette'>
-                    <ColorItem theme="dark" colorValue="red" />
-                    <ColorItem theme="light" colorValue="orange" />
-                    <ColorItem theme="light" colorValue="yellow" />
-                    <ColorItem theme="dark" colorValue="green" />
-                    <ColorItem theme="light" colorValue="turquoise" />
-                    <ColorItem theme="dark" colorValue="blue" />
-                    <ColorItem theme="light" colorValue="lilac" />
-                    <ColorItem theme="light" colorValue="pink" />
-                    <ColorItem theme="light" colorValue="white" />
-                    <ColorItem theme="dark" colorValue="gray" />
-                    <ColorItem theme="dark" colorValue="black" />
-                    <ColorItem theme="dark" colorValue="brown" />
+            <div className={`color-filter ${isOpen ? "active" : ""}`}>
+                <div className={"color-filter-title"} onClick={toggleDropdown}>
+                    <h3>Colors</h3>
+                    {isOpen ? <FaCaretUp className='color-filter-ic' /> : <FaCaretDown className='color-filter-ic' />}
                 </div>
-            </div>)}
-        </div>
+                {isOpen && 
+                (<div className='color-filter-value' ref={dropdownListRef}>
+                    <CheckBox value="Grayscale" checkboxStatue={isGrayscale} setCheckedStatus={setIsGrayscale}></CheckBox>
+                    <CheckBox value="Transparent" checkboxStatue={isTransparent} setCheckedStatus={setIsTransparent}></CheckBox>
+                    <tempColorContext.Provider value={{tempColorList ,setTempColorList}}>
+                        <div className='color-filter-palette'>
+                            <ColorItem theme="dark" colorValue="red" />
+                            <ColorItem theme="light" colorValue="orange" />
+                            <ColorItem theme="light" colorValue="yellow" />
+                            <ColorItem theme="dark" colorValue="green" />
+                            <ColorItem theme="light" colorValue="turquoise" />
+                            <ColorItem theme="dark" colorValue="blue" />
+                            <ColorItem theme="light" colorValue="lilac" />
+                            <ColorItem theme="light" colorValue="pink" />
+                            <ColorItem theme="light" colorValue="white" />
+                            <ColorItem theme="dark" colorValue="gray" />
+                            <ColorItem theme="dark" colorValue="black" />
+                            <ColorItem theme="dark" colorValue="brown" />
+                        </div>
+                    </tempColorContext.Provider>
+                    <div className='apply-filter' onClick={updateColorList}>Apply</div>
+                </div>)}
+            </div>
     )
 }
 
--- src/components/ColorFilter/ColorItem.js
+++ src/components/ColorFilter/ColorItem.js
@@ -1,10 +1,26 @@
-import React, { useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { TiTick } from "react-icons/ti";
+import { tempColorContext } from './ColorFilter';
 
-const ColorItem = ({theme, colorValue, setColorArry}) => {
+const ColorItem = ({theme, colorValue}) => {
     const [isSelected, setIsSelected] = useState(false);
+    const {tempColorList, setTempColorList} = useContext(tempColorContext);
+
+    useEffect(() => {
+        if(tempColorList.some(color => color === colorValue)){
+            setIsSelected(true);
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [tempColorList])
 
     const toggleSelection = () => {
+        if(!isSelected){
+            setTempColorList(prevArray => [...prevArray, colorValue]);
+        }
+        else{
+            const removedColorArray = tempColorList.filter(color => color !== colorValue);
+            setTempColorList(removedColorArray);
+        }
         setIsSelected(prev => !prev);
     }
 
